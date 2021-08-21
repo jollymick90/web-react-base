9b7cbabc9ba770975364a0988281e19f50630feb
michele scarpa <scarpa.michele.90@gmail.com>
2021-08-21 18:39:06 +0200

    create my base project to start web page. Stack: - react js - typescript - tailwind - postcss

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App h-full">
+
+      Hello guys
+
+    </div>
+  );
+}
+
+
+
+// export default App;
+export default App;
